3f57a0962d7805a1c700008f99fb9c00f4869f13
304bxnjvv <benjaminantoniorodriguez04@gmail.com>
2025-12-04 11:16:40 -0300

    solution find word

--- src/main/java/cl/seiza/challenges/search/arrays/FindWordInArray.java
+++ src/main/java/cl/seiza/challenges/search/arrays/FindWordInArray.java
@@ -3,7 +3,37 @@ package cl.seiza.challenges.search.arrays;
 import cl.seiza.challenges.search.arrays.domain.Position;
 
 public class FindWordInArray {
+
     public Position find(String word, Character[] arrayWithWord) {
-        return new Position(-1, -1);
+        if (word == null || word.isEmpty() || arrayWithWord == null) {
+            return new Position(0, 0);
+        }
+
+        char[] wordChars = word.toCharArray();
+        int wordLength   = wordChars.length;
+
+        if (arrayWithWord.length < wordLength) {
+            return new Position(0, 0);
+        }
+
+        int lastStart = arrayWithWord.length - wordLength;
+
+        for (int i = 0; i <= lastStart; i++) {
+            if (matchesAt(arrayWithWord, wordChars, i)) {
+                return new Position(i, i + wordLength - 1);
+            }
+        }
+
+        return new Position(0, 0);
+    }
+
+    private boolean matchesAt(Character[] array, char[] word, int offset) {
+        for (int j = 0; j < word.length; j++) {
+            Character c = array[offset + j];
+            if (c == null || c != word[j]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
